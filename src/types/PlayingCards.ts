10b9bd0cd3dd52bb067060cacdd5ea64a64df272
fridaenoksen-dev <hello@fridaenoksen.no>
2026-09-13 15:18:55 +0200

    chore: added types for player, playingCards and PokerHand

---
+++ src/types/PlayingCards.ts
@@ -0,0 +1,8 @@
+export type Suit = "clubs" | "diamonds" | "hearts" | "spades";
+
+export type Value = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14;
+
+export type PlayingCard = {
+  suit: Suit;
+  value: Value;
+};
---
+++ src/types/PokerHand.ts
@@ -0,0 +1,11 @@
+export type PokerHand =
+  | "highCard"
+  | "pair"
+  | "twoPair"
+  | "threeOfAKind"
+  | "straight"
+  | "flush"
+  | "fullHouse"
+  | "FourOfAKind"
+  | "straightFlush"
+  | "royalFlush";
