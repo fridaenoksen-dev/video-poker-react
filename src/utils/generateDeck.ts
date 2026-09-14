a230d5d5b4c4df201be273bd9a6e05bc90ccfd02
fridaenoksen-dev <hello@fridaenoksen.no>
2026-09-14 21:04:54 +0200

    comment: JSDoc comment for generateDeck

--- src/utils/generateDeck.ts
+++ src/utils/generateDeck.ts
@@ -1,5 +1,12 @@
 import type { Suit, Value, PlayingCard } from "../types/PlayingCards";
 
+/**
+ * Generates a complete and unshuffled deck with 52 playing card.
+ * One card for every combination of suit and value.
+ *
+ * @returns An array of 52 PlayingCard objects.
+ */
+
 const suits: Suit[] = ["clubs", "diamonds", "hearts", "spades"];
 const values: Value[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
@@ -10,8 +17,3 @@ export function generateDeck(): PlayingCard[] {
 
   return combinations;
 }
-
-/* Jeg har hentet typene fra PlayingCards og satt opp funksjonen for å kombinere Suits og Values.
-Funksjonen går igjennom hver farge og for hver verdi lager den et kort. Totalt 13 av hver farge.
-Med .flatMap får jeg en samlet alle kombinasjonene til én liste, istedenfor lister i lister.
- */
